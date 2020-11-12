536197d7ecb4445736242becd516460c94ecd252
Nayeemuddin Sheriff <62983647+NayeemuddinSheriff@users.noreply.github.com>
2020-11-12 16:54:00 +0530

    Update code.js

--- code.js
+++ code.js
@@ -76,3 +76,14 @@ function validateName() {
         return true;
       }
     }
+
+/* NAV BAR */
+var header = document.getElementById("navbarCollapse");
+var btns = header.getElementsByClassName("nav-link");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
